3983d5add09ad440477b720bfcba240b8919f2a5
carlohelder <carlohelder@hotmail.com>
2016-01-19 16:16:02 +0100

    project 2 menu

--- Python_project_2.py
+++ Python_project_2.py
@@ -18,8 +18,7 @@ bright_green = (0,255,0)
 
  
 block_color = (53,115,255)
- 
-car_width = 73
+
  
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Frequency')
@@ -53,14 +52,20 @@ def game_intro():
         
         
         mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+       
         
         if 150+100 > mouse[0] > 150 and 500+50 > mouse[1] > 500:
             pygame.draw.rect(gameDisplay, bright_green, (150,500,100,50))
+            #if click[0] == 1:
+                #game_loop()
+            
+                 
         else:
             pygame.draw.rect(gameDisplay, green, (150,500,100,50))
 
         smallText = pygame.font.Font('freesansbold.ttf', 20)
-        textSurf, textRect = text_objects('Play', smallText)
+        textSurf, textRect = text_objects('Play!', smallText)
         textRect.center = ( (150+(100/2)), (500+(50/2)) )
         gameDisplay.blit(textSurf, textRect)
 
@@ -68,19 +73,32 @@ def game_intro():
         
         
         if 350+100 > mouse[0] > 350 and 500+50 > mouse[1] > 500:
-            pygame.draw.rect(gameDisplay, bright_blue, (350, 500, 100, 50))
+            pygame.draw.rect(gameDisplay, bright_blue, (350,500,100,50))
+            #if click[0] is == 1:
+                #Rules_loop()
         else: 
             pygame.draw.rect(gameDisplay, blue, (350,500,100,50))
         
         smallText = pygame.font.Font('freesansbold.ttf', 20)
-        textSurf, textRect = text_objects('Game Rules', smallText)
+        textSurf, textRect = text_objects('Rules', smallText)
         textRect.center = ( (350+(100/2)), (500+(50/2)) )
         gameDisplay.blit(textSurf, textRect)
         
+        
+        
+        
         if 550+100 > mouse[0] > 550 and 500+50 > mouse[1] > 500:    
             pygame.draw.rect(gameDisplay, bright_red, (550,500,100,50))
+            if click[0] == 1:
+                pygame.quit()
+                quit()
         else:
             pygame.draw.rect(gameDisplay, red, (550,500,100,50))
+
+        smallText = pygame.font.Font('freesansbold.ttf', 20)
+        textSurf, textRect = text_objects('Quit!', smallText)
+        textRect.center = ( (550+(100/2)), (500+(50/2)) )
+        gameDisplay.blit(textSurf, textRect)
         
             
         pygame.display.update()
