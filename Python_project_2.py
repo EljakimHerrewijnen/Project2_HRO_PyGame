0e98602d44979539c1bb5fb5102dd7a6f5a88483
carlohelder <carlohelder@hotmail.com>
2016-01-19 14:32:26 +0100

    project 2 menu (new)

--- Python_project_2.py
+++ Python_project_2.py
@@ -1,91 +1,91 @@
-import pygame
-import sys
-from menu import *
+﻿import pygame
+import time
+import random
+ 
+pygame.init()
+ 
+display_width = 800
+display_height = 600
+ 
+black = (0,0,0)
+white = (255,255,255)
+bright_blue = (0, 0, 255)
+red = (175,0,0)
+green = (0,175,0)
+blue = (30,144,255)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
+ 
+block_color = (53,115,255)
+ 
+car_width = 73
+ 
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Frequency')
+clock = pygame.time.Clock()
+
+
  
 
-## ---[ main ]------------------------------------------------------------------
-#  This function runs the entire screen and contains the main while loop
-#
-def main():
-   # Initialize Pygame
-   pygame.init()
 
-   # Create a window of 800x600 pixels
-   screen = pygame.display.set_mode((800, 600))
-
-   # Set the window caption
-   pygame.display.set_caption("Menu Example - (c) Scott Barlow")
-
-   # Create 3 diffrent menus.  One of them is only text, another one is only
-   # images, and a third is -gasp- a mix of images and text buttons!  To
-   # understand the input factors, see the menu file
-   menu = cMenu(50, 50, 20, 5, 'vertical', 100, screen,
-               [('Start Game', 1, None),
-                ('Load Game',  2, None),
-                ('Options',    3, None),
-                ('Exit',       4, None)])
-
-   # Center the menu on the draw_surface (the entire screen here)
-   menu.set_center(True, True)
-
-   # Center the menu on the draw_surface (the entire screen here)
-   menu.set_alignment('center', 'center')
-
-   # Create the state variables (make them different so that the user event is
-   # triggered at the start of the "while 1" loop so that the initial display
-   # does not wait for user input)
-   state = 0
-   prev_state = 1
-
-   # rect_list is the list of pygame.Rect's that will tell pygame where to
-   # update the screen (there is no point in updating the entire screen if only
-   # a small portion of it changed!)
-   rect_list = []
-
-   # Ignore mouse motion (greatly reduces resources when not needed)
-   pygame.event.set_blocked(pygame.MOUSEMOTION)
-
-   # The main while loop
-   while 1:
-      # Check if the state has changed, if it has, then post a user event to
-      # the queue to force the menu to be shown at least once
-      if prev_state != state:
-         pygame.event.post(pygame.event.Event(EVENT_CHANGE_STATE, key = 0))
-         prev_state = state
-
-      # Get the next event
-      e = pygame.event.wait()
-
-      # Update the menu, based on which "state" we are in - When using the menu
-      # in a more complex program, definitely make the states global variables
-      # so that you can refer to them by a name
-      if e.type == pygame.KEYDOWN or e.type == EVENT_CHANGE_STATE:
-         if state == 0:
-            rect_list, state = menu.update(e, state)
-         elif state == 1:
-            print ('Start Game!')
-            state = 0
-         elif state == 2:
-            print ('Load Game!')
-            state = 0
-         elif state == 3:
-            print ('Options!')
-            state = 0
-         else:
-            print ('Exit!')
-            pygame.quit()
-            sys.exit()
-
-      # Quit if the user presses the exit button
-      if e.type == pygame.QUIT:
-         pygame.quit()
-         sys.exit()
-
-      # Update the screen
-      pygame.display.update(rect_list)
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
 
-## ---[ The python script starts here! ]----------------------------------------
-# Run the script
-if __name__ == "__main__":
-   main()
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects('Frequency', largeText)
+        TextRect.center = ((display_width/2), (display_height/4))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        
+        
+        mouse = pygame.mouse.get_pos()
+        
+        if 150+100 > mouse[0] > 150 and 500+50 > mouse[1] > 500:
+            pygame.draw.rect(gameDisplay, bright_green, (150,500,100,50))
+        else:
+            pygame.draw.rect(gameDisplay, green, (150,500,100,50))
+
+        smallText = pygame.font.Font('freesansbold.ttf', 20)
+        textSurf, textRect = text_objects('Play', smallText)
+        textRect.center = ( (150+(100/2)), (500+(50/2)) )
+        gameDisplay.blit(textSurf, textRect)
+
+
+        
+        
+        if 350+100 > mouse[0] > 350 and 500+50 > mouse[1] > 500:
+            pygame.draw.rect(gameDisplay, bright_blue, (350, 500, 100, 50))
+        else: 
+            pygame.draw.rect(gameDisplay, blue, (350,500,100,50))
+        
+        smallText = pygame.font.Font('freesansbold.ttf', 20)
+        textSurf, textRect = text_objects('Game Rules', smallText)
+        textRect.center = ( (350+(100/2)), (500+(50/2)) )
+        gameDisplay.blit(textSurf, textRect)
+        
+        if 550+100 > mouse[0] > 550 and 500+50 > mouse[1] > 500:    
+            pygame.draw.rect(gameDisplay, bright_red, (550,500,100,50))
+        else:
+            pygame.draw.rect(gameDisplay, red, (550,500,100,50))
+        
+            
+        pygame.display.update()
+        clock.tick(15)
+
+game_intro()
+
+
