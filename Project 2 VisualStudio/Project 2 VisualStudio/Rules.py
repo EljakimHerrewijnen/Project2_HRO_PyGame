d4f3b12f994433cdd8601f22c145d03689717651
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-01-21 16:08:51 +0100

    Rules
    Rules, in habbo's plaats

--- Project 2 VisualStudio/Project 2 VisualStudio/Rules.py
+++ Project 2 VisualStudio/Project 2 VisualStudio/Rules.py
@@ -27,8 +27,8 @@ def SandRules():
     block_color = (53,115,255)
     
     def text_objects(text, font):
-        textSurface = font.render(text, True, black)
-        return textSurface, textSurface.get_rect()
+        TextSurface = font.render(text, True, black)
+        return TextSurface, TextSurface.get_rect()
  
     gameDisplay = pygame.display.set_mode((display_width,display_height))
     pygame.display.set_caption('Frequency')
@@ -43,54 +43,27 @@ def SandRules():
             if event.type == pygame.QUIT:
                 Rules = False        
      
-
-
-        #gameDisplay.fill(white)
+        gameDisplay.fill(white)
         largeText = pygame.font.Font('freesansbold.ttf', 115)
+        smallText = pygame.font.Font('freesansbold.ttf', 14)
+        #Title
+        TextSurf, TextRect = text_objects('Rules', largeText)
+        TextRect.center = ((display_width/2), (display_height/4))
 
-        
-        
+
+        #Text/Rules
+        TextSurf, TextRect = text_objects('These are the game rules:   The game has 4 different biomes, each of these biomes has a special advantage. Every player gets one biome, which biome the player gets is random. "', smallText)
+        TextRect.center = ((display_width/3), (display_height/3))
+        gameDisplay.blit(TextSurf, TextRect)
+
+
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-       
-        
-        if 150+100 > mouse[0] > 150 and 500+50 > mouse[1] > 500:
-            pygame.draw.rect(gameDisplay, bright_green, (150,500,100,50))
-            if click[0] == 1:
-                game_loop()
-            
-                 
-        else:
-            pygame.draw.rect(gameDisplay, green, (150,500,100,50))
 
-        smallText = pygame.font.Font('freesansbold.ttf', 20)
-        textSurf, textRect = text_objects('Play!', smallText)
-        textRect.center = ( (150+(100/2)), (500+(50/2)) )
-        gameDisplay.blit(textSurf, textRect)
-
-
-        
-        
-        if 350+100 > mouse[0] > 350 and 500+50 > mouse[1] > 500:
-            pygame.draw.rect(gameDisplay, bright_blue, (350,500,100,50))
-            #if click[0]  == 1:
-                #rules()
-        else: 
-            pygame.draw.rect(gameDisplay, blue, (350,500,100,50))
-        
-        smallText = pygame.font.Font('freesansbold.ttf', 20)
-        textSurf, textRect = text_objects('Rules', smallText)
-        textRect.center = ( (350+(100/2)), (500+(50/2)) )
-        gameDisplay.blit(textSurf, textRect)
-        
-        
-        
-        
         if 550+100 > mouse[0] > 550 and 500+50 > mouse[1] > 500:    
             pygame.draw.rect(gameDisplay, bright_red, (550,500,100,50))
-            if click[0] == 1:
-                pygame.quit()
-                quit()
+        if click[0] == 1:
+                winning = False
         else:
             pygame.draw.rect(gameDisplay, red, (550,500,100,50))
 
@@ -98,6 +71,7 @@ def SandRules():
         textSurf, textRect = text_objects('Quit!', smallText)
         textRect.center = ( (550+(100/2)), (500+(50/2)) )
         gameDisplay.blit(textSurf, textRect)
+
             
         pygame.display.update()
         clock.tick(15)
