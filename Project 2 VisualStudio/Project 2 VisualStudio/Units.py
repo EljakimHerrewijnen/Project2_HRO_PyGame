77be467273be9f4be622a9c8f5044b27271bb23b
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-01-22 11:25:16 +0100

    PlayerNode, UnitNode
    PlayerNode, UnitNode aangemaakt en werkent

---
+++ Project 2 VisualStudio/Project 2 VisualStudio/Units.py
@@ -0,0 +1,11 @@
+import pygame
+from Gameloop import *
+                            #Code van Eljakim
+
+class Units():
+    def __init__(self, id, unittype, position):
+        self.id = id
+        self.unittype = unittype
+        self.position = position
+
+    
