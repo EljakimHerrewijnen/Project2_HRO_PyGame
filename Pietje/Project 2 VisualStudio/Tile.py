55386f0e3e11fca070d275caec6e4e5ba7a2ef6c
Joostdw1 <joostbenik@hotmail.com>
2016-02-03 16:13:50 +0100

    nieuwe muziek

--- Pietje/Project 2 VisualStudio/Tile.py
+++ Pietje/Project 2 VisualStudio/Tile.py
@@ -217,7 +217,7 @@ def tile_loop(Playerslist, copy_Playerslist):
     screen = pygame.display.set_mode((Mapwidth * Tilesize + 250, Mapheight * Tilesize))
     done = False
     clock = pygame.time.Clock()
-    pygame.mixer.music.load('Cipher2.mp3')
+    pygame.mixer.music.load('Birth of a Hero Extended.mp3')
     pygame.mixer.music.play(0)
 
     bgmap = pygame.image.load("content/map895.jpg")
@@ -396,7 +396,6 @@ def tile_loop(Playerslist, copy_Playerslist):
                 #if copy_AddUnit.Tail.IsEmpty == True:
                 #    copy_AddUnit.Tail = copy2_AddUnit
                 print("Searching for a battle...")
-<<<<<<< HEAD
                 copy_AddUnit = copy_AddUnit.Tail
                 if currentUnit.Value.position == copy_AddUnit.Value.position:
                     if copy_AddUnit.Value.AttackValue > currentUnit.Value.AttackValue:
@@ -432,7 +431,7 @@ def tile_loop(Playerslist, copy_Playerslist):
         #    n += 1
         #    AddUnit = AddUnit.Tail
 
-=======
+        """             #eljakim
                 AddUnit = AddUnit.Tail
                 if APlayersPosition.Value.position == AddUnit.Value.position:
                     APlayersPosition2 = AddUnit.Value
@@ -447,7 +446,8 @@ def tile_loop(Playerslist, copy_Playerslist):
                         print("This is a battle test")
                 elif APlayersPosition is not AddUnit.Value.position:
                             print("boe")
->>>>>>> origin/master
+                            """
+
         if pygame.mouse.get_pressed()[0] and pygame.mouse.get_pos()[0] < (Mapwidth * Tilesize):
             mouse_x = math.floor(pygame.mouse.get_pos()[0] / Tilesize) * Tilesize
             mouse_y = math.floor(pygame.mouse.get_pos()[1] / Tilesize) * Tilesize
@@ -686,14 +686,6 @@ def tile_loop(Playerslist, copy_Playerslist):
                 currentPl_tanks = currentPlayerList.Value.Tanks
                 currentPl_barracks = currentPlayerList.Value.Barracks
                 currentPl_boats = currentPlayerList.Value.Boats
-<<<<<<< HEAD
-=======
-                #currentPl_soldiers = currentPlayerList.Value.Soldiers
-                #currentPl_robots = currentPlayerList.Value.Robots
-                #currentPl_tanks = currentPlayerList.Value.Tanks
-                #currentPl_barracks = currentPlayerList.Value.Barracks
-                #currentPl_boats = currentPlayerList.Value.Boats
->>>>>>> origin/master
                 nr_movements = 0
                 #global currentPl_id
                 print("Player id = " + str(currentPl_id) + " Player name = " + currentPL_name)
